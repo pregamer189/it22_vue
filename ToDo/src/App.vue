384831c63c9fc9cb226d219a177f61925b2c3731
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2024-10-04 09:41:54 +0300

    Todo-projektet, kan lägga till uppgifter att göra

---
+++ ToDo/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const list = ref([
+    { text: 'Köp mjölk', done: false },
+    { text: 'Köp ägg', done: true },
+    { text: 'Köp bröd', done: false },
+])
+const itemText = ref('')
+
+function addItem() {
+    let listItem = { text: itemText.value, done: false }
+    list.value.push(listItem)
+    itemText.value = ''
+}
+</script>
+
+<template>
+    <header>
+        <h1>ToDo-lista</h1>
+        <input type="text" placeholder="Ny uppgift" v-model="itemText" />
+        <button v-on:click="addItem">Lägg till</button>
+    </header>
+
+    <main>
+        <h3>Att göra:</h3>
+        <ul>
+
+            <template v-for="itm in list" v-bind:key="itm">
+                <li>{{ itm.text }}</li>
+            </template>
+        </ul>
+    </main>
+</template>
+
+<style scoped></style>
