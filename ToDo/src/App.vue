452d0c2cecffe5bc02facafe3de7a3510f1d0da4
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2024-10-07 10:31:36 +0300

    Todo-appen klar

--- ToDo/src/App.vue
+++ ToDo/src/App.vue
@@ -2,17 +2,19 @@
 import { ref } from 'vue';
 
 
-const list = ref([
-    { text: 'Köp mjölk', done: false },
-    { text: 'Köp ägg', done: true },
-    { text: 'Köp bröd', done: false },
-])
+const list = ref([])
 const itemText = ref('')
 
 function addItem() {
     let listItem = { text: itemText.value, done: false }
     list.value.push(listItem)
     itemText.value = ''
+}
+
+function radera(listItem) {
+    list.value = list.value.filter((obj) => {
+        return obj !== listItem
+    })
 }
 </script>
 
@@ -24,14 +26,62 @@ function addItem() {
     </header>
 
     <main>
-        <h3>Att göra:</h3>
+        <h3 id="todo">Att göra:</h3>
         <ul>
 
             <template v-for="itm in list" v-bind:key="itm">
-                <li>{{ itm.text }}</li>
+                <li class="todoItm" v-if="!itm.done" @click="itm.done = !itm.done">{{ itm.text }}
+                    <span title="Radera" @click="radera(itm)">x</span>
+                </li>
+            </template>
+        </ul>
+        <h3 id="klart">Klart:</h3>
+        <ul>
+
+            <template v-for="itm in list" v-bind:key="itm">
+                <li class="todoItm done" v-if="itm.done" @click="itm.done = !itm.done">{{ itm.text }}
+                    <span title="Radera" @click="radera(itm)">x</span>
+                </li>
             </template>
         </ul>
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.todoItm {
+    font-weight: bold;
+    padding: .5em;
+    list-style-type: none;
+    cursor: pointer;
+}
+
+.todoItm:nth-child(even) {
+    background-color: lightblue;
+}
+
+.todoItm:nth-child(odd) {
+    background-color: aqua;
+}
+
+.done {
+    text-decoration: solid black line-through;
+}
+
+#klart {
+    color: greenyellow;
+}
+
+#todo {
+    color: pink;
+}
+
+li span {
+    display: inline-block;
+    position: absolute;
+    right: 3em;
+    background-color: red;
+    color: wheat;
+    padding-left: .5em;
+    padding-right: .5em;
+}
+</style>
